08f05176d48f7d5c7a1cb6b4cbc4e2da5c8cd0bf
João Paulo Lima <jptick@gmail.com>
2020-09-16 09:45:34 -0300

    Create new Type

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,18 @@
 import{ Request, Response} from 'express';
+import createUser from './services/CreateUser';
 
 function helloWorld(request: Request, response: Response) {
+  const user = createUser({ 
+    email:'jptick@gmail.com',
+    password:'123',
+    techs:[
+      'Node',
+      'ReactJS',
+      'React Native',
+      {title:'javascript', experience:100},
+    ],
+  });
+
   return response.json({message:'Hello World'});
 }
 
